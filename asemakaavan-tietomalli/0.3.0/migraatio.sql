87539a8823b1da4243f354cc4ba0c7cc8672f9fd
Mikael Vaaltola <mvaaltola@gmail.com>
2020-10-09 10:40:22 +0300

    Add 0.3.0 SQL, graphics, migrations

---
+++ asemakaavan-tietomalli/0.3.0/migraatio.sql
@@ -0,0 +1,34 @@
+-- Diff code generated with pgModeler (PostgreSQL Database Modeler)
+-- pgModeler version: 0.9.2
+-- Diff date: 2020-10-09 10:38:44
+-- Source model: qaava-asemakaava
+-- Database: ak020
+-- PostgreSQL version: 11.0
+
+-- [ Diff summary ]
+-- Dropped objects: 0
+-- Created objects: 0
+-- Changed objects: 8
+-- Truncated tables: 0
+
+SET search_path=public,pg_catalog,asemakaavat,koodistot,kaavan_lisatiedot;
+-- ddl-end --
+
+
+-- [ Changed objects ] --
+COMMENT ON EXTENSION postgis IS '';
+-- ddl-end --
+COMMENT ON EXTENSION "uuid-ossp" IS '';
+-- ddl-end --
+ALTER TABLE asemakaavat.asemakaava ALTER COLUMN geom TYPE geometry(MULTIPOLYGONZ, 3877);
+-- ddl-end --
+ALTER TABLE asemakaavat.kaavaelementti ALTER COLUMN geom_polygon TYPE geometry(MULTIPOLYGONZ, 3877);
+-- ddl-end --
+ALTER TABLE asemakaavat.kaavaelementti ALTER COLUMN geom_line TYPE geometry(MULTILINESTRINGZ, 3877);
+-- ddl-end --
+ALTER TABLE asemakaavat.kaavaelementti ALTER COLUMN geom_point TYPE geometry(MULTIPOINTZ, 3877);
+-- ddl-end --
+ALTER TABLE asemakaavat.maankayttoalue ALTER COLUMN geom TYPE geometry(MULTIPOLYGONZ, 3877);
+-- ddl-end --
+ALTER TABLE asemakaavat.osa_alue ALTER COLUMN geom TYPE geometry(MULTIPOLYGONZ, 3877);
+-- ddl-end --
