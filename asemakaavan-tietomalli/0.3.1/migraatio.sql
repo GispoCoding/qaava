822663cfdb7524da2319ffd31143955c5eaf05b6
Mikael Vaaltola <mvaaltola@gmail.com>
2020-10-20 13:34:55 +0300

    Fix migrations

--- asemakaavan-tietomalli/0.3.1/migraatio.sql
+++ asemakaavan-tietomalli/0.3.1/migraatio.sql
@@ -1,34 +1,187 @@
--- Diff code generated with pgModeler (PostgreSQL Database Modeler)
--- pgModeler version: 0.9.2
--- Diff date: 2020-10-20 11:54:19
--- Source model: qaava-asemakaava
--- Database: ak030
--- PostgreSQL version: 11.0
-
--- [ Diff summary ]
--- Dropped objects: 0
--- Created objects: 0
--- Changed objects: 8
--- Truncated tables: 0
-
 SET search_path=public,pg_catalog,asemakaavat,koodistot,kaavan_lisatiedot;
--- ddl-end --
 
 
--- [ Changed objects ] --
-COMMENT ON EXTENSION postgis IS '';
--- ddl-end --
-COMMENT ON EXTENSION "uuid-ossp" IS '';
--- ddl-end --
-ALTER TABLE asemakaavat.asemakaava ALTER COLUMN geom TYPE geometry(MULTIPOLYGONZ, 3877);
--- ddl-end --
-ALTER TABLE asemakaavat.kaavaelementti ALTER COLUMN geom_polygon TYPE geometry(MULTIPOLYGONZ, 3877);
--- ddl-end --
-ALTER TABLE asemakaavat.kaavaelementti ALTER COLUMN geom_line TYPE geometry(MULTILINESTRINGZ, 3877);
--- ddl-end --
-ALTER TABLE asemakaavat.kaavaelementti ALTER COLUMN geom_point TYPE geometry(MULTIPOINTZ, 3877);
--- ddl-end --
-ALTER TABLE asemakaavat.maankayttoalue ALTER COLUMN geom TYPE geometry(MULTIPOLYGONZ, 3877);
--- ddl-end --
-ALTER TABLE asemakaavat.osa_alue ALTER COLUMN geom TYPE geometry(MULTIPOLYGONZ, 3877);
--- ddl-end --
+ALTER TABLE asemakaavat.maankayttoalue DROP CONSTRAINT IF EXISTS asemakaava_fk CASCADE;
+ALTER TABLE asemakaavat.maankayttoalue ADD CONSTRAINT asemakaava_fk FOREIGN KEY (uuid_asemakaava)
+REFERENCES asemakaavat.asemakaava (uuid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE koodistot.kaavamaarays DROP CONSTRAINT IF EXISTS osa_alue_fk CASCADE;
+ALTER TABLE koodistot.kaavamaarays ADD CONSTRAINT osa_alue_fk FOREIGN KEY (uuid_osa_alue)
+REFERENCES asemakaavat.osa_alue (uuid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE asemakaavat.asemakaava DROP CONSTRAINT IF EXISTS taustakartta_fk CASCADE;
+ALTER TABLE asemakaavat.asemakaava ADD CONSTRAINT taustakartta_fk FOREIGN KEY (gid_taustakartta)
+REFERENCES kaavan_lisatiedot.taustakartta (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE koodistot.kaavaelementti_tyyppi DROP CONSTRAINT IF EXISTS hsrcl_fk CASCADE;
+ALTER TABLE koodistot.kaavaelementti_tyyppi ADD CONSTRAINT hsrcl_fk FOREIGN KEY (gid_hsrcl)
+REFERENCES koodistot.hsrcl (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE koodistot.osa_alue_tyyppi DROP CONSTRAINT IF EXISTS hsrcl_fk CASCADE;
+ALTER TABLE koodistot.osa_alue_tyyppi ADD CONSTRAINT hsrcl_fk FOREIGN KEY (gid_hsrcl)
+REFERENCES koodistot.hsrcl (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE asemakaavat.osa_alue DROP CONSTRAINT IF EXISTS asemakaava_fk CASCADE;
+ALTER TABLE asemakaavat.osa_alue ADD CONSTRAINT asemakaava_fk FOREIGN KEY (uuid_asemakaava)
+REFERENCES asemakaavat.asemakaava (uuid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE asemakaavat.kaavaelementti DROP CONSTRAINT IF EXISTS asemakaava_fk CASCADE;
+ALTER TABLE asemakaavat.kaavaelementti ADD CONSTRAINT asemakaava_fk FOREIGN KEY (uuid_asemakaava)
+REFERENCES asemakaavat.asemakaava (uuid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE asemakaavat.asemakaava DROP CONSTRAINT IF EXISTS kieli_fk CASCADE;
+ALTER TABLE asemakaavat.asemakaava ADD CONSTRAINT kieli_fk FOREIGN KEY (gid_kieli)
+REFERENCES koodistot.kieli (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE asemakaavat.asemakaava DROP CONSTRAINT IF EXISTS vaihetieto_fk CASCADE;
+ALTER TABLE asemakaavat.asemakaava ADD CONSTRAINT vaihetieto_fk FOREIGN KEY (gid_vaihetieto)
+REFERENCES koodistot.vaihetieto (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE asemakaavat.asemakaava DROP CONSTRAINT IF EXISTS kaavatyyppi_fk CASCADE;
+ALTER TABLE asemakaavat.asemakaava ADD CONSTRAINT kaavatyyppi_fk FOREIGN KEY (gid_kaavatyyppi)
+REFERENCES koodistot.kaavatyyppi (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE asemakaavat.kaavaelementti DROP CONSTRAINT IF EXISTS kaavaelementti_tyyppi_fk CASCADE;
+ALTER TABLE asemakaavat.kaavaelementti ADD CONSTRAINT kaavaelementti_tyyppi_fk FOREIGN KEY (gid_kaavaelementti_tyyppi)
+REFERENCES koodistot.kaavaelementti_tyyppi (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE asemakaavat.osa_alue DROP CONSTRAINT IF EXISTS osa_alue_tyyppi_fk CASCADE;
+ALTER TABLE asemakaavat.osa_alue ADD CONSTRAINT osa_alue_tyyppi_fk FOREIGN KEY (gid_osa_alue_tyyppi)
+REFERENCES koodistot.osa_alue_tyyppi (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE asemakaavat.maankayttoalue DROP CONSTRAINT IF EXISTS maankayttoluokka_fk CASCADE;
+ALTER TABLE asemakaavat.maankayttoalue ADD CONSTRAINT maankayttoluokka_fk FOREIGN KEY (gid_maankayttoluokka)
+REFERENCES koodistot.maankayttoluokka (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE kaavan_lisatiedot.dokumentti DROP CONSTRAINT IF EXISTS kieli_fk CASCADE;
+ALTER TABLE kaavan_lisatiedot.dokumentti ADD CONSTRAINT kieli_fk FOREIGN KEY (gid_kieli)
+REFERENCES koodistot.kieli (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE kaavan_lisatiedot.dokumentti DROP CONSTRAINT IF EXISTS dokumenttityyppi_fk CASCADE;
+ALTER TABLE kaavan_lisatiedot.dokumentti ADD CONSTRAINT dokumenttityyppi_fk FOREIGN KEY (gid_dokumenttityyppi)
+REFERENCES koodistot.dokumenttityyppi (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE koodistot.maankayttoluokka DROP CONSTRAINT IF EXISTS hilucs_fk CASCADE;
+ALTER TABLE koodistot.maankayttoluokka ADD CONSTRAINT hilucs_fk FOREIGN KEY (gid_hilucs)
+REFERENCES koodistot.hilucs (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE kaavan_lisatiedot.many_dokumentti_has_many_asemakaava DROP CONSTRAINT IF EXISTS dokumentti_fk CASCADE;
+ALTER TABLE kaavan_lisatiedot.many_dokumentti_has_many_asemakaava ADD CONSTRAINT dokumentti_fk FOREIGN KEY (gid_dokumentti)
+REFERENCES kaavan_lisatiedot.dokumentti (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE kaavan_lisatiedot.many_dokumentti_has_many_asemakaava DROP CONSTRAINT IF EXISTS asemakaava_fk CASCADE;
+ALTER TABLE kaavan_lisatiedot.many_dokumentti_has_many_asemakaava ADD CONSTRAINT asemakaava_fk FOREIGN KEY (uuid_asemakaava)
+REFERENCES asemakaavat.asemakaava (uuid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE asemakaavat.asemakaava DROP CONSTRAINT IF EXISTS dokumentti_fk CASCADE;
+ALTER TABLE asemakaavat.asemakaava ADD CONSTRAINT dokumentti_fk FOREIGN KEY (gid_dokumentti)
+REFERENCES kaavan_lisatiedot.dokumentti (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE kaavan_lisatiedot.numeerinen_lisatieto DROP CONSTRAINT IF EXISTS numeerinen_merkintatyyppi_fk CASCADE;
+ALTER TABLE kaavan_lisatiedot.numeerinen_lisatieto ADD CONSTRAINT numeerinen_merkintatyyppi_fk FOREIGN KEY (gid_numeerinen_merkintatyyppi)
+REFERENCES koodistot.numeerinen_merkintatyyppi (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE asemakaavat.many_maankayttoalue_has_many_numeerinen_lisatieto DROP CONSTRAINT IF EXISTS maankayttoalue_fk CASCADE;
+ALTER TABLE asemakaavat.many_maankayttoalue_has_many_numeerinen_lisatieto ADD CONSTRAINT maankayttoalue_fk FOREIGN KEY (uuid_maankayttoalue)
+REFERENCES asemakaavat.maankayttoalue (uuid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE asemakaavat.many_maankayttoalue_has_many_numeerinen_lisatieto DROP CONSTRAINT IF EXISTS numeerinen_lisatieto_fk CASCADE;
+ALTER TABLE asemakaavat.many_maankayttoalue_has_many_numeerinen_lisatieto ADD CONSTRAINT numeerinen_lisatieto_fk FOREIGN KEY (gid_numeerinen_lisatieto)
+REFERENCES kaavan_lisatiedot.numeerinen_lisatieto (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE asemakaavat.many_osa_alue_has_many_numeerinen_lisatieto DROP CONSTRAINT IF EXISTS osa_alue_fk CASCADE;
+ALTER TABLE asemakaavat.many_osa_alue_has_many_numeerinen_lisatieto ADD CONSTRAINT osa_alue_fk FOREIGN KEY (uuid_osa_alue)
+REFERENCES asemakaavat.osa_alue (uuid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE asemakaavat.many_osa_alue_has_many_numeerinen_lisatieto DROP CONSTRAINT IF EXISTS numeerinen_lisatieto_fk CASCADE;
+ALTER TABLE asemakaavat.many_osa_alue_has_many_numeerinen_lisatieto ADD CONSTRAINT numeerinen_lisatieto_fk FOREIGN KEY (gid_numeerinen_lisatieto)
+REFERENCES kaavan_lisatiedot.numeerinen_lisatieto (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE asemakaavat.many_kaavaelementti_has_many_numeerinen_lisatieto DROP CONSTRAINT IF EXISTS kaavaelementti_fk CASCADE;
+ALTER TABLE asemakaavat.many_kaavaelementti_has_many_numeerinen_lisatieto ADD CONSTRAINT kaavaelementti_fk FOREIGN KEY (uuid_kaavaelementti)
+REFERENCES asemakaavat.kaavaelementti (uuid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE asemakaavat.many_kaavaelementti_has_many_numeerinen_lisatieto DROP CONSTRAINT IF EXISTS numeerinen_lisatieto_fk CASCADE;
+ALTER TABLE asemakaavat.many_kaavaelementti_has_many_numeerinen_lisatieto ADD CONSTRAINT numeerinen_lisatieto_fk FOREIGN KEY (gid_numeerinen_lisatieto)
+REFERENCES kaavan_lisatiedot.numeerinen_lisatieto (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE koodistot.kaavaelementti_tyyppi DROP CONSTRAINT IF EXISTS geometria_tyyppi_fk CASCADE;
+ALTER TABLE koodistot.kaavaelementti_tyyppi ADD CONSTRAINT geometria_tyyppi_fk FOREIGN KEY (gid_geometria_tyyppi)
+REFERENCES koodistot.geometria_tyyppi (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE asemakaavat.many_maankayttoalue_has_many_kaavamaarays DROP CONSTRAINT IF EXISTS maankayttoalue_fk CASCADE;
+ALTER TABLE asemakaavat.many_maankayttoalue_has_many_kaavamaarays ADD CONSTRAINT maankayttoalue_fk FOREIGN KEY (uuid_maankayttoalue)
+REFERENCES asemakaavat.maankayttoalue (uuid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE asemakaavat.many_maankayttoalue_has_many_kaavamaarays DROP CONSTRAINT IF EXISTS kaavamaarays_fk CASCADE;
+ALTER TABLE asemakaavat.many_maankayttoalue_has_many_kaavamaarays ADD CONSTRAINT kaavamaarays_fk FOREIGN KEY (uuid_kaavamaarays)
+REFERENCES koodistot.kaavamaarays (uuid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE asemakaavat.many_asemakaava_has_many_kaavamaarays DROP CONSTRAINT IF EXISTS asemakaava_fk CASCADE;
+ALTER TABLE asemakaavat.many_asemakaava_has_many_kaavamaarays ADD CONSTRAINT asemakaava_fk FOREIGN KEY (uuid_asemakaava)
+REFERENCES asemakaavat.asemakaava (uuid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE asemakaavat.many_asemakaava_has_many_kaavamaarays DROP CONSTRAINT IF EXISTS kaavamaarays_fk CASCADE;
+ALTER TABLE asemakaavat.many_asemakaava_has_many_kaavamaarays ADD CONSTRAINT kaavamaarays_fk FOREIGN KEY (uuid_kaavamaarays)
+REFERENCES koodistot.kaavamaarays (uuid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE asemakaavat.many_kaavaelementti_has_many_kaavamaarays DROP CONSTRAINT IF EXISTS kaavaelementti_fk CASCADE;
+ALTER TABLE asemakaavat.many_kaavaelementti_has_many_kaavamaarays ADD CONSTRAINT kaavaelementti_fk FOREIGN KEY (uuid_kaavaelementti)
+REFERENCES asemakaavat.kaavaelementti (uuid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE asemakaavat.many_kaavaelementti_has_many_kaavamaarays DROP CONSTRAINT IF EXISTS kaavamaarays_fk CASCADE;
+ALTER TABLE asemakaavat.many_kaavaelementti_has_many_kaavamaarays ADD CONSTRAINT kaavamaarays_fk FOREIGN KEY (uuid_kaavamaarays)
+REFERENCES koodistot.kaavamaarays (uuid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE asemakaavat.kaavaelementti DROP CONSTRAINT IF EXISTS dokumentti_fk CASCADE;
+ALTER TABLE asemakaavat.kaavaelementti ADD CONSTRAINT dokumentti_fk FOREIGN KEY (gid_dokumentti)
+REFERENCES kaavan_lisatiedot.dokumentti (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE asemakaavat.maankayttoalue DROP CONSTRAINT IF EXISTS dokumentti_fk CASCADE;
+ALTER TABLE asemakaavat.maankayttoalue ADD CONSTRAINT dokumentti_fk FOREIGN KEY (gid_dokumentti)
+REFERENCES kaavan_lisatiedot.dokumentti (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE asemakaavat.osa_alue DROP CONSTRAINT IF EXISTS dokumentti_fk CASCADE;
+ALTER TABLE asemakaavat.osa_alue ADD CONSTRAINT dokumentti_fk FOREIGN KEY (gid_dokumentti)
+REFERENCES kaavan_lisatiedot.dokumentti (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+
+
+
+
--- yleiskaavan-tietomalli/0.3.1/migraatio.sql
+++ yleiskaavan-tietomalli/0.3.1/migraatio.sql
@@ -1,32 +1,177 @@
--- Diff code generated with pgModeler (PostgreSQL Database Modeler)
--- pgModeler version: 0.9.2
--- Diff date: 2020-10-20 11:53:12
--- Source model: qaava-yleiskaava
--- Database: yk030
--- PostgreSQL version: 11.0
-
--- [ Diff summary ]
--- Dropped objects: 0
--- Created objects: 0
--- Changed objects: 7
--- Truncated tables: 0
-
 SET search_path=public,pg_catalog,yleiskaava,koodistot,kaavan_lisatiedot;
--- ddl-end --
+
+ALTER TABLE yleiskaava.maankayttoalue DROP CONSTRAINT IF EXISTS yleiskaava_fk CASCADE;
+ALTER TABLE yleiskaava.maankayttoalue ADD CONSTRAINT yleiskaava_fk FOREIGN KEY (uuid_yleiskaava)
+REFERENCES yleiskaava.yleiskaava (uuid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
 
 
--- [ Changed objects ] --
-COMMENT ON EXTENSION postgis IS '';
--- ddl-end --
-COMMENT ON EXTENSION "uuid-ossp" IS '';
--- ddl-end --
-ALTER TABLE yleiskaava.yleiskaava ALTER COLUMN geom TYPE geometry(MULTIPOLYGONZ, 3877);
--- ddl-end --
-ALTER TABLE yleiskaava.kaavaelementti ALTER COLUMN geom_polygon TYPE geometry(MULTIPOLYGONZ, 3877);
--- ddl-end --
-ALTER TABLE yleiskaava.kaavaelementti ALTER COLUMN geom_line TYPE geometry(MULTILINESTRINGZ, 3877);
--- ddl-end --
-ALTER TABLE yleiskaava.kaavaelementti ALTER COLUMN geom_point TYPE geometry(MULTIPOINTZ, 3877);
--- ddl-end --
-ALTER TABLE yleiskaava.maankayttoalue ALTER COLUMN geom TYPE geometry(MULTIPOLYGONZ, 3877);
--- ddl-end --
+ALTER TABLE yleiskaava.yleiskaava DROP CONSTRAINT IF EXISTS taustakartta_fk CASCADE;
+ALTER TABLE yleiskaava.yleiskaava ADD CONSTRAINT taustakartta_fk FOREIGN KEY (gid_taustakartta)
+REFERENCES kaavan_lisatiedot.taustakartta (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE koodistot.kaavaelementti_tyyppi DROP CONSTRAINT IF EXISTS hsrcl_fk CASCADE;
+ALTER TABLE koodistot.kaavaelementti_tyyppi ADD CONSTRAINT hsrcl_fk FOREIGN KEY (gid_hsrcl)
+REFERENCES koodistot.hsrcl (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE yleiskaava.kaavaelementti DROP CONSTRAINT IF EXISTS yleiskaava_fk CASCADE;
+ALTER TABLE yleiskaava.kaavaelementti ADD CONSTRAINT yleiskaava_fk FOREIGN KEY (uuid_yleiskaava)
+REFERENCES yleiskaava.yleiskaava (uuid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE yleiskaava.yleiskaava DROP CONSTRAINT IF EXISTS kieli_fk CASCADE;
+ALTER TABLE yleiskaava.yleiskaava ADD CONSTRAINT kieli_fk FOREIGN KEY (gid_kieli)
+REFERENCES koodistot.kieli (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE yleiskaava.yleiskaava DROP CONSTRAINT IF EXISTS vaihetieto_fk CASCADE;
+ALTER TABLE yleiskaava.yleiskaava ADD CONSTRAINT vaihetieto_fk FOREIGN KEY (gid_vaihetieto)
+REFERENCES koodistot.vaihetieto (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE yleiskaava.yleiskaava DROP CONSTRAINT IF EXISTS kaavatyyppi_fk CASCADE;
+ALTER TABLE yleiskaava.yleiskaava ADD CONSTRAINT kaavatyyppi_fk FOREIGN KEY (gid_kaavatyyppi)
+REFERENCES koodistot.kaavatyyppi (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE yleiskaava.kaavaelementti DROP CONSTRAINT IF EXISTS kaavaelementti_tyyppi_fk CASCADE;
+ALTER TABLE yleiskaava.kaavaelementti ADD CONSTRAINT kaavaelementti_tyyppi_fk FOREIGN KEY (gid_kaavaelementti_tyyppi)
+REFERENCES koodistot.kaavaelementti_tyyppi (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE yleiskaava.maankayttoalue DROP CONSTRAINT IF EXISTS maankayttoluokka_fk CASCADE;
+ALTER TABLE yleiskaava.maankayttoalue ADD CONSTRAINT maankayttoluokka_fk FOREIGN KEY (gid_maankayttoluokka)
+REFERENCES koodistot.maankayttoluokka (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE kaavan_lisatiedot.dokumentti DROP CONSTRAINT IF EXISTS kieli_fk CASCADE;
+ALTER TABLE kaavan_lisatiedot.dokumentti ADD CONSTRAINT kieli_fk FOREIGN KEY (gid_kieli)
+REFERENCES koodistot.kieli (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE kaavan_lisatiedot.dokumentti DROP CONSTRAINT IF EXISTS dokumenttityyppi_fk CASCADE;
+ALTER TABLE kaavan_lisatiedot.dokumentti ADD CONSTRAINT dokumenttityyppi_fk FOREIGN KEY (gid_dokumenttityyppi)
+REFERENCES koodistot.dokumenttityyppi (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE koodistot.maankayttoluokka DROP CONSTRAINT IF EXISTS hilucs_fk CASCADE;
+ALTER TABLE koodistot.maankayttoluokka ADD CONSTRAINT hilucs_fk FOREIGN KEY (gid_hilucs)
+REFERENCES koodistot.hilucs (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE kaavan_lisatiedot.many_dokumentti_has_many_yleiskaava DROP CONSTRAINT IF EXISTS dokumentti_fk CASCADE;
+ALTER TABLE kaavan_lisatiedot.many_dokumentti_has_many_yleiskaava ADD CONSTRAINT dokumentti_fk FOREIGN KEY (gid_dokumentti)
+REFERENCES kaavan_lisatiedot.dokumentti (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE kaavan_lisatiedot.many_dokumentti_has_many_yleiskaava DROP CONSTRAINT IF EXISTS yleiskaava_fk CASCADE;
+ALTER TABLE kaavan_lisatiedot.many_dokumentti_has_many_yleiskaava ADD CONSTRAINT yleiskaava_fk FOREIGN KEY (uuid_yleiskaava)
+REFERENCES yleiskaava.yleiskaava (uuid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE yleiskaava.yleiskaava DROP CONSTRAINT IF EXISTS dokumentti_fk CASCADE;
+ALTER TABLE yleiskaava.yleiskaava ADD CONSTRAINT dokumentti_fk FOREIGN KEY (gid_dokumentti)
+REFERENCES kaavan_lisatiedot.dokumentti (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE kaavan_lisatiedot.numeerinen_lisatieto DROP CONSTRAINT IF EXISTS numeerinen_merkintatyyppi_fk CASCADE;
+ALTER TABLE kaavan_lisatiedot.numeerinen_lisatieto ADD CONSTRAINT numeerinen_merkintatyyppi_fk FOREIGN KEY (gid_numeerinen_merkintatyyppi)
+REFERENCES koodistot.numeerinen_merkintatyyppi (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE yleiskaava.many_maankayttoalue_has_many_numeerinen_lisatieto DROP CONSTRAINT IF EXISTS maankayttoalue_fk CASCADE;
+ALTER TABLE yleiskaava.many_maankayttoalue_has_many_numeerinen_lisatieto ADD CONSTRAINT maankayttoalue_fk FOREIGN KEY (uuid_maankayttoalue)
+REFERENCES yleiskaava.maankayttoalue (uuid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE yleiskaava.many_maankayttoalue_has_many_numeerinen_lisatieto DROP CONSTRAINT IF EXISTS numeerinen_lisatieto_fk CASCADE;
+ALTER TABLE yleiskaava.many_maankayttoalue_has_many_numeerinen_lisatieto ADD CONSTRAINT numeerinen_lisatieto_fk FOREIGN KEY (gid_numeerinen_lisatieto)
+REFERENCES kaavan_lisatiedot.numeerinen_lisatieto (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE yleiskaava.many_kaavaelementti_has_many_numeerinen_lisatieto DROP CONSTRAINT IF EXISTS kaavaelementti_fk CASCADE;
+ALTER TABLE yleiskaava.many_kaavaelementti_has_many_numeerinen_lisatieto ADD CONSTRAINT kaavaelementti_fk FOREIGN KEY (uuid_kaavaelementti)
+REFERENCES yleiskaava.kaavaelementti (uuid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE yleiskaava.many_kaavaelementti_has_many_numeerinen_lisatieto DROP CONSTRAINT IF EXISTS numeerinen_lisatieto_fk CASCADE;
+ALTER TABLE yleiskaava.many_kaavaelementti_has_many_numeerinen_lisatieto ADD CONSTRAINT numeerinen_lisatieto_fk FOREIGN KEY (gid_numeerinen_lisatieto)
+REFERENCES kaavan_lisatiedot.numeerinen_lisatieto (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE yleiskaava.many_kaavaelementti_has_many_teema DROP CONSTRAINT IF EXISTS kaavaelementti_fk CASCADE;
+ALTER TABLE yleiskaava.many_kaavaelementti_has_many_teema ADD CONSTRAINT kaavaelementti_fk FOREIGN KEY (uuid_kaavaelementti)
+REFERENCES yleiskaava.kaavaelementti (uuid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE yleiskaava.many_kaavaelementti_has_many_teema DROP CONSTRAINT IF EXISTS teema_fk CASCADE;
+ALTER TABLE yleiskaava.many_kaavaelementti_has_many_teema ADD CONSTRAINT teema_fk FOREIGN KEY (gid_teema)
+REFERENCES koodistot.teema (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE koodistot.kaavaelementti_tyyppi DROP CONSTRAINT IF EXISTS geometria_tyyppi_fk CASCADE;
+ALTER TABLE koodistot.kaavaelementti_tyyppi ADD CONSTRAINT geometria_tyyppi_fk FOREIGN KEY (gid_geometria_tyyppi)
+REFERENCES koodistot.geometria_tyyppi (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE yleiskaava.many_maankayttoalue_has_many_teema DROP CONSTRAINT IF EXISTS maankayttoalue_fk CASCADE;
+ALTER TABLE yleiskaava.many_maankayttoalue_has_many_teema ADD CONSTRAINT maankayttoalue_fk FOREIGN KEY (uuid_maankayttoalue)
+REFERENCES yleiskaava.maankayttoalue (uuid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE yleiskaava.many_maankayttoalue_has_many_teema DROP CONSTRAINT IF EXISTS teema_fk CASCADE;
+ALTER TABLE yleiskaava.many_maankayttoalue_has_many_teema ADD CONSTRAINT teema_fk FOREIGN KEY (gid_teema)
+REFERENCES koodistot.teema (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE yleiskaava.many_yleiskaava_has_many_teema DROP CONSTRAINT IF EXISTS yleiskaava_fk CASCADE;
+ALTER TABLE yleiskaava.many_yleiskaava_has_many_teema ADD CONSTRAINT yleiskaava_fk FOREIGN KEY (uuid_yleiskaava)
+REFERENCES yleiskaava.yleiskaava (uuid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE yleiskaava.many_yleiskaava_has_many_teema DROP CONSTRAINT IF EXISTS teema_fk CASCADE;
+ALTER TABLE yleiskaava.many_yleiskaava_has_many_teema ADD CONSTRAINT teema_fk FOREIGN KEY (gid_teema)
+REFERENCES koodistot.teema (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE yleiskaava.maankayttoalue DROP CONSTRAINT IF EXISTS dokumentti_fk CASCADE;
+ALTER TABLE yleiskaava.maankayttoalue ADD CONSTRAINT dokumentti_fk FOREIGN KEY (gid_dokumentti)
+REFERENCES kaavan_lisatiedot.dokumentti (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE yleiskaava.kaavaelementti DROP CONSTRAINT IF EXISTS dokumentti_fk CASCADE;
+ALTER TABLE yleiskaava.kaavaelementti ADD CONSTRAINT dokumentti_fk FOREIGN KEY (gid_dokumentti)
+REFERENCES kaavan_lisatiedot.dokumentti (gid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE yleiskaava.many_yleiskaava_has_many_kaavamaarays DROP CONSTRAINT IF EXISTS yleiskaava_fk CASCADE;
+ALTER TABLE yleiskaava.many_yleiskaava_has_many_kaavamaarays ADD CONSTRAINT yleiskaava_fk FOREIGN KEY (uuid_yleiskaava)
+REFERENCES yleiskaava.yleiskaava (uuid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE yleiskaava.many_yleiskaava_has_many_kaavamaarays DROP CONSTRAINT IF EXISTS kaavamaarays_fk CASCADE;
+ALTER TABLE yleiskaava.many_yleiskaava_has_many_kaavamaarays ADD CONSTRAINT kaavamaarays_fk FOREIGN KEY (uuid_kaavamaarays)
+REFERENCES koodistot.kaavamaarays (uuid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE yleiskaava.many_maankayttoalue_has_many_kaavamaarays DROP CONSTRAINT IF EXISTS maankayttoalue_fk CASCADE;
+ALTER TABLE yleiskaava.many_maankayttoalue_has_many_kaavamaarays ADD CONSTRAINT maankayttoalue_fk FOREIGN KEY (uuid_maankayttoalue)
+REFERENCES yleiskaava.maankayttoalue (uuid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE yleiskaava.many_maankayttoalue_has_many_kaavamaarays DROP CONSTRAINT IF EXISTS kaavamaarays_fk CASCADE;
+ALTER TABLE yleiskaava.many_maankayttoalue_has_many_kaavamaarays ADD CONSTRAINT kaavamaarays_fk FOREIGN KEY (uuid_kaavamaarays)
+REFERENCES koodistot.kaavamaarays (uuid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE yleiskaava.many_kaavaelementti_has_many_kaavamaarays DROP CONSTRAINT IF EXISTS kaavaelementti_fk CASCADE;
+ALTER TABLE yleiskaava.many_kaavaelementti_has_many_kaavamaarays ADD CONSTRAINT kaavaelementti_fk FOREIGN KEY (uuid_kaavaelementti)
+REFERENCES yleiskaava.kaavaelementti (uuid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+ALTER TABLE yleiskaava.many_kaavaelementti_has_many_kaavamaarays DROP CONSTRAINT IF EXISTS kaavamaarays_fk CASCADE;
+ALTER TABLE yleiskaava.many_kaavaelementti_has_many_kaavamaarays ADD CONSTRAINT kaavamaarays_fk FOREIGN KEY (uuid_kaavamaarays)
+REFERENCES koodistot.kaavamaarays (uuid) MATCH FULL
+ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
