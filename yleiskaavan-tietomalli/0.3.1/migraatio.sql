de0c104224dac08d4f39f026d14056b26e339a0c
Mikael Vaaltola <mvaaltola@gmail.com>
2020-10-20 11:55:40 +0300

    Add migrations, update version

---
+++ yleiskaavan-tietomalli/0.3.1/migraatio.sql
@@ -0,0 +1,32 @@
+-- Diff code generated with pgModeler (PostgreSQL Database Modeler)
+-- pgModeler version: 0.9.2
+-- Diff date: 2020-10-20 11:53:12
+-- Source model: qaava-yleiskaava
+-- Database: yk030
+-- PostgreSQL version: 11.0
+
+-- [ Diff summary ]
+-- Dropped objects: 0
+-- Created objects: 0
+-- Changed objects: 7
+-- Truncated tables: 0
+
+SET search_path=public,pg_catalog,yleiskaava,koodistot,kaavan_lisatiedot;
+-- ddl-end --
+
+
+-- [ Changed objects ] --
+COMMENT ON EXTENSION postgis IS '';
+-- ddl-end --
+COMMENT ON EXTENSION "uuid-ossp" IS '';
+-- ddl-end --
+ALTER TABLE yleiskaava.yleiskaava ALTER COLUMN geom TYPE geometry(MULTIPOLYGONZ, 3877);
+-- ddl-end --
+ALTER TABLE yleiskaava.kaavaelementti ALTER COLUMN geom_polygon TYPE geometry(MULTIPOLYGONZ, 3877);
+-- ddl-end --
+ALTER TABLE yleiskaava.kaavaelementti ALTER COLUMN geom_line TYPE geometry(MULTILINESTRINGZ, 3877);
+-- ddl-end --
+ALTER TABLE yleiskaava.kaavaelementti ALTER COLUMN geom_point TYPE geometry(MULTIPOINTZ, 3877);
+-- ddl-end --
+ALTER TABLE yleiskaava.maankayttoalue ALTER COLUMN geom TYPE geometry(MULTIPOLYGONZ, 3877);
+-- ddl-end --
